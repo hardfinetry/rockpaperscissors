11ede9fc2b1314402615b6464267aba1e4a1f232
hardfinetry-whitey <221923048+hardfinetry@users.noreply.github.com>
2026-05-26 10:25:50 +0800

    solve rps-ui, buttons not disabled yet upon gameOver

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ const humanChoiceButtons = document.querySelector("#humanChoiceButtons");
 const results = document.querySelector("#results");
 const humanScoreId = document.querySelector("#humanScoreId");
 const computerScoreId = document.querySelector("#computerScoreId");
+const humanChose = document.querySelector("#humanChose");
+const computerChose = document.querySelector('#computerChose');
+const isGameOver = document.querySelector("#isGameOver");
 
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
     let random = Math.random();
-    if (random >= 0 & random <= 0.33) {
-        results.append("Computer chose rock");
+    if (random >= 0 && random <= 0.33) {
         return "rock";
     }
-    else if (random > 0.33 & random <= 0.66) {
-        results.append("Computer chose paper");
+    else if (random > 0.33 && random <= 0.66) {
         return "paper";
     }
-    else if (random > 0.66 & random <= 1) {
-        results.append("Computer chose scissors");
+    else if (random > 0.66 && random <= 1) {
         return "scissors";
     }
 };
@@ -25,15 +25,15 @@ function getComputerChoice() {
 function playRound(humanChoice) {
     let humanSelection = humanChoice;
     let computerSelection = getComputerChoice();
+    humanChose.textContent = `You chose ${humanSelection}`;
+    computerChose.textContent = `Computer chose ${computerSelection}`;
 
     if (
         (humanSelection === "rock" && computerSelection === "paper") ||
         (humanSelection === "paper" && computerSelection === "scissors") ||
         (humanSelection === "scissors" && computerSelection === "rock")
     ) {
-        results.append(`You chose ${humanSelection} and computer chose ${computerSelection}, you lose!`);
         computerScore += 1;
-        // results.append(`Human score is ${humanScore} and Computer score is ${computerScore}`);
         humanScoreId.textContent = "Human Score: " + humanScore;
         computerScoreId.textContent = "Computer Score: " + computerScore;
     }
@@ -42,18 +42,16 @@ function playRound(humanChoice) {
         (humanSelection === "scissors" && computerSelection === "paper") ||
         (humanSelection === "rock" && computerSelection === "scissors")
     ) {
-        results.append(`You chose ${humanSelection} and computer chose ${computerSelection}, you win!`);
         humanScore += 1;
-        // results.append(`Human score is ${humanScore} and Computer score is ${computerScore}`);
         humanScoreId.textContent = "Human Score: " + humanScore;
         computerScoreId.textContent = "Computer Score: " + computerScore;
     }
-    else {
-        results.append(`You chose ${humanSelection} and computer chose ${computerSelection}, it's a draw!`);
-        results.append(`Human score is ${humanScore} and Computer score is ${computerScore}`);
+
+    if (humanScore == 5 || computerScore == 5) {
+        isGameOver.textContent = "Game Over!"
     }
 };
 
 humanChoiceButtons.addEventListener("click", (event) => {
-    playRound(event.target.id)
+    playRound(event.target.id);
 });
